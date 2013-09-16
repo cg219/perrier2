2f62a7eb7f0740d6c7dedb651b67e71e413b1c8c
boaz <btopol@gmail.com>
2013-09-16 02:49:28 -0400

    Made the search global
    Although there were some plugins already released both free or premium,
    they each had flaws in one way or another for the global search.

--- search.php
+++ search.php
@@ -1,51 +1,53 @@
-<? 
-	get_template_part("consts");
-	get_header();
+<?
+get_template_part("consts");
+get_header();
 ?>
 <body>
-	<div class="navbar navbar-inverse navbar-static-top">
-		<div class="container wrap">
-			<? get_template_part("nav"); ?>
-		</div>
-	</div>
-	<div class="div wrap" id="wrapper">
-		<div class="container" id="main">
-			<h2>Search Results for "<? echo get_search_query(); ?>"</h2>
-			<?
-				if(have_posts()): while(have_posts()): the_post();
+    <div class="navbar navbar-inverse navbar-static-top">
+        <div class="container wrap">
+            <? get_template_part("nav"); ?>
+        </div>
+    </div>
+    <div class="div wrap" id="wrapper">
+        <div class="container" id="main">
+            <?
+            $searchfor = get_search_query(); // Get the search query for display in a headline
+            ?>
 
-				$cats = array();
+            <h1>Search results for <?php echo $searchfor; ?>;'</h1>
 
-				$terms2 = wp_get_post_terms(get_the_ID(), "primary_category");
+            <?php
+            $query_string = esc_attr($query_string); // Escaping search queries to eliminate potential MySQL-injections
+            $blogs = get_blog_list(0, 'all');
+            foreach ($blogs as $blog):
+                switch_to_blog($blog['blog_id']);
+                $search = new WP_Query($query_string);
+                if ($search->found_posts > 0) {
+                    foreach ($search->posts as $post) {
+                        setup_postdata($post);
+                        $author_data = get_userdata(get_the_author_meta('ID'));
+                        ?>
+                        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                            <p>
+                                <span><?php the_time('Y/m/d') ?></span>
+                                By <?php the_author_posts_link(); ?> </p>
+                            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                            <div id="entry-content"><?php
+            the_excerpt();
+                        ?>
+                            </div>
 
-				for( $i=0; $i < count($terms2); $i++){
-					array_push($cats, $terms2[$i]->name);
-				}
-			?>
-			<div class="media article">
-				<a href="<? the_permalink(); ?>" class="pull-left">
-					<? 
-						if( has_post_thumbnail() ) :
-							$img = wp_get_attachment_image_src( get_post_thumbnail_id(), 'thumb');
-					?>
-						<img src="<? echo $img[0]; ?>" class="media-object" />
-					<? else :?>
-					<img src="" alt="">
-					<? endif; ?>
-				</a>
-				<div class="media-body">
-					<h5><? echo join(", ", $cats); ?></h5>
-					<h3><a href="<? the_permalink(); ?>"><? the_title(); ?></a></h3>
-					<p><? the_excerpt(); ?><a href="<? the_permalink(); ?>">Read More</a></p>
-				</div>
-			</div>
-			<?
-				endwhile;
-				else:
-			?>
-			<h1>NOthing Found</h1>
-			<? endif; ?>
-		</div>
-		<? get_sidebar(); ?>
-	</div>
-	<? get_footer(); ?>
+                        </div>
+            <?php
+        }
+    }
+endforeach;
+restore_current_blog(); // Reset settings to the current blog
+?>
+        </div>
+            <? get_sidebar(); ?>
+    </div>
+            <? get_footer(); ?>
+
+
+
