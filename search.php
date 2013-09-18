3e48c85f7074f6ca79a2586b38457baf6aeaee46
Clemente Gomez <zomegpro@gmail.com>
2013-09-18 14:20:56 -0400

    Restyled Search

--- search.php
+++ search.php
@@ -1,47 +1,53 @@
-<?
-get_template_part("consts");
-get_header();
+<? 
+    get_template_part("consts");
+    get_header();
 ?>
 <body>
     <? get_template_part("nav"); ?>
     <div class="div wrap" id="wrapper">
         <div class="container" id="main">
+            <h2>Search Results for "<? echo get_search_query(); ?>"</h2>
             <?
-            $searchfor = get_search_query(); // Get the search query for display in a headline
+                global $query_string;
+                $query_string = esc_attr($query_string); // Escaping search queries to eliminate potential MySQL-injections
+                $blogs = get_blog_list(0, 'all');
+                foreach ($blogs as $blog):
+                    switch_to_blog($blog['blog_id']);
+                    $search = new WP_Query($query_string);
+                    if ($search->found_posts > 0) :
+                        foreach ($search->posts as $post):
+                            setup_postdata($post);
+                            $cats = array();
+                            $terms2 = wp_get_post_terms(get_the_ID(), "primary_category");
+                            for( $i=0; $i < count($terms2); $i++){
+                                array_push($cats, $terms2[$i]);
+                            }
             ?>
-
-            <h1>Search results for <?php echo $searchfor; ?>;'</h1>
-
-            <?php
-            $query_string = esc_attr($query_string); // Escaping search queries to eliminate potential MySQL-injections
-            $blogs = get_blog_list(0, 'all');
-            foreach ($blogs as $blog):
-                switch_to_blog($blog['blog_id']);
-                $search = new WP_Query($query_string);
-                if ($search->found_posts > 0) {
-                    foreach ($search->posts as $post) {
-                        setup_postdata($post);
-                        $author_data = get_userdata(get_the_author_meta('ID'));
-                        ?>
-                        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                            <p>
-                                <span><?php the_time('Y/m/d') ?></span>
-                                By <?php the_author_posts_link(); ?> </p>
-                            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                            <div id="entry-content"><?php the_excerpt(); ?>
-                            </div>
-
-                        </div>
-            <?php
-        }
-    }
-endforeach;
-restore_current_blog(); // Reset settings to the current blog
-?>
+            <div class="media article">
+                <a href="<? the_permalink(); ?>" class="pull-left">
+                    <? 
+                        if( has_post_thumbnail() ) :
+                            $img = wp_get_attachment_image_src( get_post_thumbnail_id(), 'thumb');
+                    ?>
+                        <img src="<? echo $img[0]; ?>" class="media-object" />
+                    <? else :?>
+                    <img src="" alt="">
+                    <? endif; ?>
+                </a>
+                <div class="media-body">
+                    <? $thisCat = get_term_link($cats[0]); ?>
+                    <h5><a href="<? echo $thisCat->errors ? "" : $thisCat;?>"><? echo $cats[0]->name; ?></a></h5>
+                    <h3><a href="<? the_permalink(); ?>"><? the_title(); ?></a></h3>
+                    <p><? echo $post->post_excerpt; ?> <a class="readmore" href="<? the_permalink(); ?>">Read More</a></p>
+                </div>
+            </div>
+            <?
+                        endforeach;
+                    endif;
+                    restore_current_blog(); // Reset settings to the current blog
+                endforeach;
+            ?>
         </div>
-            <? get_sidebar(); ?>
+        <? get_sidebar(); ?>
     </div>
-            <? get_footer(); ?>
-
-
-
+    <? get_footer(); ?>
