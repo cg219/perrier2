37bede3574265da7a79a74bb8308f6a30308533d
Clemente Gomez <zomegpro@gmail.com>
2013-09-19 17:47:09 -0400

    Refresh Hotspot Backend

--- kreate/pages/clear_data.php
+++ kreate/pages/clear_data.php
@@ -1,12 +1,19 @@
 <?
-	if(isset($_POST["kreate-global-ops"])){
-	}
+	if(isset($_POST["kreate-hotspot-refresh"])):
+		kreate_create_hotspot_list();
+?>
+	<div class="alert alert-dismissable alert-success">
+		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+		<strong>Hotspot Cache Updated!</strong>
+	</div>
+<?
+	endif;
 ?>
 
 <div class="container">
-	<h2>Clear Data</h2>
+	<h2>Data Cleanse</h2>
 	<form action="" role="form" method="post">
-		<button type="submit" class="btn btn-primary">Clear Global Blog Options</button>
-		<input type="hidden" name="kreate-global-ops" value="true" />
+		<button type="submit" class="btn btn-primary">Refresh Cached Hotspots</button>
+		<input type="hidden" name="kreate-hotspot-refresh" value="true" />
 	</form>
 </div>
