0e05995baf5d5d8a195af956714e0f088a66d89a
Clemente Gomez <zomegpro@gmail.com>
2013-09-20 11:34:52 -0400

    Updated Titles

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title><? wp_title( "|", true, "right"); ?></title>
+	<title><? wp_title( "|", true, "right"); ?> <? bloginfo("name"); ?></title>
 	<link rel="stylesheet" href="<? echo theme_uri; ?>/css/bootstrap.min.css">
 	<link rel="stylesheet" href="<? echo theme_uri; ?>/style.css">
 	<? wp_head(); ?>
